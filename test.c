bc4754b0cbc5de8fe8ad3745dadd3f942aec1622
Pandoxo <wozniak.hubert.pb+github@gmail.com>
2025-04-06 22:50:05 +0200

    working on 4.0

--- test.c
+++ test.c
@@ -3,6 +3,43 @@
 #include <string.h>
 
 
+struct Section;
+struct Array;
+
+typedef struct {
+    char *key;
+    char *value;
+} KeyValuePair;
+
+
+typedef struct 
+{
+    Section* data;
+    size_t used;
+    size_t size;
+} Array;
+
+typedef struct {
+    char *name;
+    Array *pairs;
+    int numberOfPairs;
+} Section;
+
+void initArray(Array* a,size_t initialSize)
+{
+    a->data = malloc(initialSize * sizeof(Section));
+    a->used =0;
+    a->size = initialSize;
+}
+void insertArray(Array* a,Section data)
+{
+    if (a->used == a->size)
+    {
+        a->size *= 2;
+        a->data = realloc(a->data,a->size * sizeof(Section));
+    }
+    a->data[a->used++] = data;
+}
 
 void invalidSymbols(char* txt,char* name)
 {
@@ -33,19 +70,24 @@ int main(int argc, char* argv[])
     FILE *fptr;
     char content[63];
     char path[20];
+
+    //Initialize dynamic array
+    Array sections;
+    initArray(&sections,100);
     
     strcpy(path,argv[1]);
     fptr = fopen(path,"r");
     
-
+    int i = -1; //numer of section
+    
     char* tokPtr;
     tokPtr = strtok(argv[2],".");
     char* section = tokPtr;
     tokPtr = strtok(NULL,",");
     char* key= tokPtr;
-    printf("%s\n",section);
-    printf("%s\n",key);
-    printf("%s\n\n",path);
+    // printf("%s\n",section);
+    // printf("%s\n",key);
+    // printf("%s\n\n",path);
     if (fptr != NULL)
     {
         printf("\nfile opened\n");
@@ -58,23 +100,48 @@ int main(int argc, char* argv[])
             if (firstLetter == '[')
             {
                 //Reading section
+                Array atributes;
+                initArray(&atributes,5);
+
                 char* tokSectionPtr = strtok(content,"[]");
+                Section sec;
+                sec.name = strdup(tokSectionPtr);
+
+                ifmissing(sec.name,"section");
+                invalidSymbols(sec.name,"section");
+                sec.numberOfPairs = 0;
+
+                insertArray(&sections,sec);
+                i++;
                 readSection = strdup(tokSectionPtr);
                 ifmissing(readSection,"section");
                 invalidSymbols(readSection,"section");
+
             }else if (firstLetter == '\n')
             {
                 continue;
             }
             else
             {
+                KeyValuePair pair;
+
                 //Reading key and value
                 char* tokPtr2;
+                
                 tokPtr2 = strtok(content," ");
+
+                pair.key = strdup(tokPtr2);
                 readkey = strdup(tokPtr2);
                 tokPtr2 = strtok(NULL," "); // "="
                 tokPtr2 = strtok(NULL," \n");
                 readValue = strdup(tokPtr2);
+                pair.value = strdup(tokPtr2);
+                
+                ifmissing(pair.key,"key");
+                ifmissing(pair.value,"value");
+                invalidSymbols(pair.key,"key");
+                invalidSymbols(pair.value,"value");
+                // insertArray(sections.data[i].pairs
                 ifmissing(readkey,"key");
                 ifmissing(readValue,"value");
                 invalidSymbols(readkey,"key");
