756ce68879c737330f7eccf16792381d1cfe8bf1
Pandoxo <wozniak.hubert.pb+github@gmail.com>
2025-04-06 18:23:01 +0200

    3.5

--- test.c
+++ test.c
@@ -3,6 +3,31 @@
 #include <string.h>
 
 
+
+void invalidSymbols(char* txt,char* name)
+{
+    
+    for(int i = 0; txt[i] != '\0';i++)
+    {
+        char c = txt[i];
+        if ( !((c>47 && c<58) || (c>64 && c<133) || (c>96 &&c<123) || (c=='-')) )
+        {
+            printf("invalid %s: %s\n",name,txt);
+            exit(1);
+        }
+    }
+}
+
+void ifmissing(char* arg,char* name)
+{
+    if(arg == NULL)
+    {
+        printf("%s missing",name);
+        exit(1);
+    }
+}
+
+
 int main(int argc, char* argv[])
 {
     FILE *fptr;
@@ -12,7 +37,7 @@ int main(int argc, char* argv[])
     strcpy(path,argv[1]);
     fptr = fopen(path,"r");
     
-    
+
     char* tokPtr;
     tokPtr = strtok(argv[2],".");
     char* section = tokPtr;
@@ -23,30 +48,46 @@ int main(int argc, char* argv[])
     printf("%s\n\n",path);
     if (fptr != NULL)
     {
+        printf("\nfile opened\n");
         char* readSection;
         while (fgets(content,63,fptr))
         {
             char firstLetter = content[0];    
-            char* readkey;
-            char* readValue;
+            char* readkey = NULL;
+            char* readValue = NULL;
             if (firstLetter == '[')
             {
+                //Reading section
                 char* tokSectionPtr = strtok(content,"[]");
                 readSection = strdup(tokSectionPtr);
-            }else
+                ifmissing(readSection,"section");
+                invalidSymbols(readSection,"section");
+            }else if (firstLetter == '\n')
             {
+                continue;
+            }
+            else
+            {
+                //Reading key and value
                 char* tokPtr2;
                 tokPtr2 = strtok(content," ");
                 readkey = strdup(tokPtr2);
                 tokPtr2 = strtok(NULL," "); // "="
-                tokPtr2 = strtok(NULL," ");
+                tokPtr2 = strtok(NULL," \n");
                 readValue = strdup(tokPtr2);
+                ifmissing(readkey,"key");
+                ifmissing(readValue,"value");
+                invalidSymbols(readkey,"key");
+                invalidSymbols(readValue,"value");
+
+
+            }
             
+
             if (readkey != NULL && readSection != NULL)
                 if (strcmp(readSection,section) ==0 && strcmp(readkey,key) ==0)
                     printf("%s %s %s\n",readSection,readkey,readValue);
-            }
-       }    
+        }    
     }
     else
         printf("file open unsuccessful");
