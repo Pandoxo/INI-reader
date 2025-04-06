9a306a32f172a85dfc622c087cab676e49283cff
Pandoxo <wozniak.hubert.pb+github@gmail.com>
2025-04-06 02:09:05 +0200

    3.0

--- test.c
+++ test.c
@@ -6,55 +6,46 @@
 int main(int argc, char* argv[])
 {
     FILE *fptr;
-    fptr = fopen("example-3.0.ini.txt","r");
     char content[63];
-
     char path[20];
+    
     strcpy(path,argv[1]);
-
-
+    fptr = fopen(path,"r");
+    
+    
     char* tokPtr;
     tokPtr = strtok(argv[2],".");
     char* section = tokPtr;
     tokPtr = strtok(NULL,",");
     char* key= tokPtr;
-    free(tokPtr);
     printf("%s\n",section);
     printf("%s\n",key);
     printf("%s\n\n",path);
     if (fptr != NULL)
     {
-       while (fgets(content,63,fptr))
-       {
-            char firstLetter = content[0];
-
-            
-            char readSection[30];
-            char readkey[30];
-            char readValue[30];
+        char* readSection;
+        while (fgets(content,63,fptr))
+        {
+            char firstLetter = content[0];    
+            char* readkey;
+            char* readValue;
             if (firstLetter == '[')
             {
-                int i = 0;
-                memset(readSection,0,30);
-
-                while (content[i+1] !=']')
-                {
-                    readSection[i] = content[i+1];
-                    i++;
-                }
+                char* tokSectionPtr = strtok(content,"[]");
+                readSection = strdup(tokSectionPtr);
             }else
             {
-                char* tokPtr;
-                tokPtr = strtok(content," ");
-                char* readkey = tokPtr;
-                tokPtr = strtok(NULL," ");
-                tokPtr = strtok(NULL," ");
-                readValue = *tokPtr
-
-            }   
-            if (strcmp(readSection,section) ==0)
-
-                    printf("%s %s\n",readSection,readkey);
+                char* tokPtr2;
+                tokPtr2 = strtok(content," ");
+                readkey = strdup(tokPtr2);
+                tokPtr2 = strtok(NULL," "); // "="
+                tokPtr2 = strtok(NULL," ");
+                readValue = strdup(tokPtr2);
+            
+            if (readkey != NULL && readSection != NULL)
+                if (strcmp(readSection,section) ==0 && strcmp(readkey,key) ==0)
+                    printf("%s %s %s\n",readSection,readkey,readValue);
+            }
        }    
     }
     else
